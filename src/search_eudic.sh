68df0e1c6a3d6a9d0a9da2768c9a20527075c857
Hanley Lee <hanley.lei@gmail.com>
2021-11-20 18:29:51 +0800

    [update] Update logic

--- src/search_eudic.sh
+++ src/search_eudic.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-eudicID=$(osascript -e 'id of app "Eudic"')
+if [[ -d /Applications/Eudb_en_free.app ]]; then
+    eudicID=$(osascript -e 'id of app "Eudb_en_free"')
+elif [[ -d /Applications/Eudic.app ]]; then
+    eudicID=$(osascript -e 'id of app "Eudic"')
+fi
+
+if [[ -z "$eudicID" ]]; then
+osascript <<EOF
+display dialog "Please install EuDic"
+EOF
+   exit
+fi
 
 osascript <<EOF
 tell application "System Events"
--- src/speak_eudic.sh
+++ src/speak_eudic.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-eudicID=$(osascript -e 'id of app "Eudic"')
+if [[ -d /Applications/Eudb_en_free.app ]]; then
+    eudicID=$(osascript -e 'id of app "Eudb_en_free"')
+elif [[ -d /Applications/Eudic.app ]]; then
+    eudicID=$(osascript -e 'id of app "Eudic"')
+fi
+
+if [[ -z "$eudicID" ]]; then
+osascript <<EOF
+display dialog "Please install EuDic"
+EOF
+   exit
+fi
 
 osascript <<EOF
 tell application id "$eudicID"
