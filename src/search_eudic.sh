ca9d309704d6a1e734002f3902d7773cdf2249d7
Hanley Lee <hanley.lei@gmail.com>
2021-11-22 16:51:52 +0800

    [adapt] Adapt for Eudb_en.app

--- src/search_eudic.sh
+++ src/search_eudic.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ -d /Applications/Eudb_en_free.app ]]; then
-    eudicID=$(osascript -e 'id of app "Eudb_en_free"')
-elif [[ -d /Applications/Eudic.app ]]; then
-    eudicID=$(osascript -e 'id of app "Eudic"')
-fi
+Eudic_ID=$(osascript -e 'id of app "Eudb_en_free"' 2>/dev/null) || \
+    Eudic_ID=$(osascript -e 'id of app "Eudb_en"' 2>/dev/null) || \
+    Eudic_ID=$(osascript -e 'id of app "Eudic"' 2>/dev/null)
 
-if [[ -z "$eudicID" ]]; then
+if [[ -z "$Eudic_ID" ]]; then
 osascript <<EOF
 display dialog "Please install EuDic"
 EOF
@@ -15,8 +13,8 @@ fi
 
 osascript <<EOF
 tell application "System Events"
-    do shell script "open -b $eudicID"
-    tell application id "$eudicID"
+    do shell script "open -b $Eudic_ID"
+    tell application id "$Eudic_ID"
         activate
         show dic with word "$1"
     end tell
